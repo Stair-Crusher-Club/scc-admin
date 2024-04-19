8c143038c93a0033fe463195c3984f19a0d20b40
양현석 <fourwingsy@gmail.com>
2024-04-19 20:07:09 +0900

    chore: add prod image host

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "scc-accessibility-images.s3.ap-northeast-2.amazonaws.com" },
+      { protocol: "https", hostname: "scc-prod-accessibility-images.s3.ap-northeast-2.amazonaws.com" },
       { protocol: "https", hostname: "scc-dev-accessibility-images.s3.ap-northeast-2.amazonaws.com" },
       { protocol: "https", hostname: "scc-dev-accessibility-images-2.s3.ap-northeast-2.amazonaws.com" },
     ],
