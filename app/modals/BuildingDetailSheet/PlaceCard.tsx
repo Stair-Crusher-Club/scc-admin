61f4bbab00dbef1a6726a7e86ce68985f46a7f12
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-04-29 14:18:53 +0900

    feature: 장소명 클릭 시 앱 열기 (#36)
    * open app in quest page
    
    * only in mobile
    
    * use app icon
    
    * chnage

--- app/modals/BuildingDetailSheet/PlaceCard.tsx
+++ app/modals/BuildingDetailSheet/PlaceCard.tsx
@@ -1,16 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import Image from "next/image"
 import { useEffect, useState } from "react"
-import { toast } from "react-toastify"
+import { useMediaQuery } from "react-responsive"
 
 import { deleteQuestTargetPlace, updateQuestStatus } from "@/lib/apis/api"
 import { QuestPlace } from "@/lib/models/quest"
+import { storage } from "@/lib/storage"
 
 import Checkbox from "@/components/Checkbox"
 
-import { storage } from "@/lib/storage"
-import naverMapIcon from "../../../public/naver_map.jpg"
 import deleteIcon from "../../../public/delete_button.png"
+import naverMapIcon from "../../../public/naver_map.jpg"
+import stairCrusherIcon from "../../../public/scc_button.png"
 import * as S from "./PlaceCard.style"
 
 interface Props {
@@ -23,6 +24,7 @@ export default function PlaceCard({ place, questId, onUpdate, onDelete }: Props)
   const [isClosed, setClosed] = useState(place.isClosed)
   const [isNotAccessible, setNotAccessible] = useState(place.isNotAccessible)
   const [authenticated, setAuthenticated] = useState<boolean>()
+  const isMobile = useMediaQuery({ maxWidth: 800 })
 
   useEffect(() => {
     const token = storage.get("token")
@@ -50,13 +52,13 @@ export default function PlaceCard({ place, questId, onUpdate, onDelete }: Props)
     }
   }
 
-  async function copyPlaceName() {
-    await navigator.clipboard.writeText(place.name)
-    toast.success("장소명을 복사했습니다.")
-  }
-
   function openInApp() {
-    toast.info("앱에서 열기 : 준비중입니다.")
+    if (isMobile) {
+      window.location.href = `stair-crusher://place/${place.placeId}`
+    } else {
+      // FIXME: 유니버셜 링크로 연동해두기
+      // window.open(`https://stair-crusher.com/place/${place.placeId}`)
+    }
   }
 
   const updateClosed = async (isClosed: boolean) => {
@@ -85,7 +87,7 @@ export default function PlaceCard({ place, questId, onUpdate, onDelete }: Props)
 
   const deletePlace = async () => {
     if (!confirm(`[${place.name}] 장소를 정말 삭제하시겠습니까?`)) return
-    await deleteQuestTargetPlace(questId, place);
+    await deleteQuestTargetPlace(questId, place)
     onDelete?.(place)
   }
 
@@ -105,22 +107,17 @@ export default function PlaceCard({ place, questId, onUpdate, onDelete }: Props)
         </S.Badges>
         <S.PlaceName>
           <span>{place.name}</span>
-          <S.Button onClick={copyPlaceName} style={{ border: "none" }}>
-            <svg width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="11" y="5" width="16" height="16" rx="4" stroke="#777" stroke-width="2" />
-              <rect x="5" y="11" width="16" height="16" rx="4" fill="white" stroke="#777" stroke-width="2" />
-            </svg>
+          <S.Button onClick={openInApp}>
+            <Image src={stairCrusherIcon} alt="계단뿌셔클럽" style={{ width: 24, height: 24 }} />
           </S.Button>
           <S.Button onClick={openNaverMap}>
             <Image src={naverMapIcon} alt="네이버 지도" style={{ width: 24, height: 24 }} />
           </S.Button>
-          {
-            authenticated ?
-              <S.Button onClick={deletePlace}>
-                <Image src={deleteIcon} alt="삭제 " style={{ width: 24, height: 24 }} />
-              </S.Button>
-              : null
-          }
+          {authenticated ? (
+            <S.Button onClick={deletePlace}>
+              <Image src={deleteIcon} alt="삭제 " style={{ width: 24, height: 24 }} />
+            </S.Button>
+          ) : null}
         </S.PlaceName>
       </S.NameColumn>
       <S.ActionsColumn>
