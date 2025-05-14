19795a6817c5a54f19fcf14fa3392e2b667393b3
ixio0330 <ixio0330@gmail.com>
2025-05-14 17:12:37 +0900

    refactor: isActive가 false일 때 Lottie 애니메이션 중지

--- app/components/Guide/TriggeredLottie/index.tsx
+++ app/components/Guide/TriggeredLottie/index.tsx
@@ -12,15 +12,21 @@ export default function TriggeredLottie({ isActive, animationData, delay }: Trig
   useEffect(() => {
     let timeoutKey: ReturnType<typeof setTimeout> | undefined
 
-    if (isActive && lottieRef.current) {
-      lottieRef.current.stop()
+    if (!lottieRef.current) {
+      return
+    }
 
-      if (delay) {
-        timeoutKey = setTimeout(() => {
-          lottieRef.current?.play()
-        }, delay)
-      } else {
-      }
+    if (!isActive) {
+      lottieRef.current.stop()
+      return
+    }
+
+    if (delay) {
+      timeoutKey = setTimeout(() => {
+        lottieRef.current?.play()
+      }, delay)
+    } else {
+      lottieRef.current.play()
     }
 
     return () => {
