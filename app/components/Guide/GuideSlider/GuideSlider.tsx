903f315fbc4431a51d550b23bbf36cb36b860a03
Hanbee <104984874+ixio0330@users.noreply.github.com>
2025-05-18 11:08:45 +0900

    feature: 정보 등록하기, 조회하기 가이드 (#38)
    * style: guide layout
    
    * style: guide typography component
    
    * style: guide header zindex 999
    
    * feat: guide slider
    
    * feat: isActive 조건에 따라 재생되는 TriggeredLottie 추가
    
    * feat: 정보 등록 가이드  이미지, lottie json 추가
    
    * feat: 정보 등록하기 페이지
    
    * feat: 종보 조회 가이드 이미지, lottie json 추가
    
    * feat: 정보 조회하기 페이지
    
    * style: 설명 간격 수정
    
    * feat: TriggeredLottie delay 옵션 추가
    
    * fix: 모바일 환경에서 Lottie 렌더 시 prev/next 버튼 렌더링 지연 해결
    
    * refactor: isActive가 false일 때 Lottie 애니메이션 중지
    
    * chore: 가이드 typo에 pretendard 적용
    
    * chore: 가이드 한 페이지에서 state로 탭 변경
    
    * style: subtitle font weight 600으로 변경
    
    * style: 버튼 영역 키우기
    
    * fix: 로티 변경
    
    * feat: 활성화된 탭 상태 url에 저장
    
    * feat: 슬라이드 스와이프 적용

---
+++ app/components/Guide/GuideSlider/GuideSlider.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { useCallback, useState } from "react"
+
+import { GuideSlideContent } from "@/constants/guide"
+import { useSwipe } from "@/hooks/useSwipe"
+
+import GuideSliderItem from "./GuideSliderItem"
+
+interface GuildSliderProps {
+  name: string
+  items: GuideSlideContent[]
+  slideItemWidth?: number
+  slideGap?: number
+}
+
+export default function GuideSlider({ name, items = [], slideItemWidth = 335, slideGap = 12 }: GuildSliderProps) {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const onClickPrev = useCallback(() => {
+    setCurrentIndex((idx) => Math.max(idx - 1, 0))
+  }, [currentIndex])
+
+  const onClickNext = useCallback(() => {
+    setCurrentIndex((idx) => Math.min(idx + 1, items.length - 1))
+  }, [currentIndex])
+
+  const { onTouchStart, onTouchEnd } = useSwipe<HTMLDivElement>({
+    onSwipeLeft: onClickNext,
+    onSwipeRight: onClickPrev,
+  })
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        overflow: "hidden",
+      }}
+      onTouchStart={onTouchStart}
+      onTouchEnd={onTouchEnd}
+    >
+      <div
+        style={{
+          display: "flex",
+          transition: "transform 0.5s ease",
+          transform: `translateX(-${currentIndex * (slideItemWidth + slideGap)}px)`,
+          gap: slideGap,
+          width: slideItemWidth,
+        }}
+      >
+        {items.map((item, idx) => (
+          <div
+            key={`${name}-slide-item-${idx}`}
+            style={{
+              flexShrink: 0,
+              width: slideItemWidth,
+              transition: "transform 0.3s, opacity 0.3s",
+            }}
+          >
+            <GuideSliderItem
+              name={name}
+              index={idx}
+              isActive={currentIndex === idx}
+              stepNumber={idx + 1}
+              slideCount={items.length}
+              onClickPrev={onClickPrev}
+              onClickNext={onClickNext}
+              hiddenPrevButton={idx === 0}
+              hiddenNextButton={idx === items.length - 1}
+              {...item}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ app/constants/guide.ts
@@ -0,0 +1,279 @@
+import buildingConquerGuide02 from "../../public/guide/register/buildingConquerGuide/buildingConquerGuide02.json"
+import buildingConquerGuide03 from "../../public/guide/register/buildingConquerGuide/buildingConquerGuide03.json"
+import buildingConquerGuide04 from "../../public/guide/register/buildingConquerGuide/buildingConquerGuide04.json"
+import buildingConquerGuide05 from "../../public/guide/register/buildingConquerGuide/buildingConquerGuide05.json"
+import placeConquerGuide03 from "../../public/guide/register/placeConquerGuide/placeConquerGuide03.json"
+import placeConquerGuide04 from "../../public/guide/register/placeConquerGuide/placeConquerGuide04.json"
+import placeConquerGuide05 from "../../public/guide/register/placeConquerGuide/placeConquerGuide05.json"
+import filterSearchGuide03 from "../../public/guide/search/filterSearchGuide/filterSearchGuide03.json"
+import filterSearchGuide04 from "../../public/guide/search/filterSearchGuide/filterSearchGuide04.json"
+import filterSearchGuide05 from "../../public/guide/search/filterSearchGuide/filterSearchGuide05.json"
+import sccRoadGuide04 from "../../public/guide/search/sccRoadGuide/sccRoadGuide04.json"
+
+type GuideSlideContentBase = {
+  title?: string
+  description: string[]
+  extraDescription?: {
+    title: string
+    description: string[]
+    descriptionStyle?: "none" | "disc"
+  }
+}
+
+export type GuideSlideContent =
+  | (GuideSlideContentBase & {
+      sourceType: "image"
+      source: string
+    })
+  | (GuideSlideContentBase & {
+      sourceType: "lottie"
+      source: object
+    })
+
+//#region 정보 등록하기
+// 장소 하나 정복하기
+export const singleConquerGuide: GuideSlideContent[] = [
+  {
+    description: ["검색창에 장소이름을 검색 후,", "검색결과에 나오는 가게를 선택해주세요."],
+    source: "/guide/register/singleConquerGuide/singleConquerGuide01.png",
+    sourceType: "image",
+  },
+  {
+    description: ["검색창을 클릭하여, 검색 창으로 진입해주세요."],
+    source: "/guide/register/singleConquerGuide/singleConquerGuide02.png",
+    sourceType: "image",
+  },
+  {
+    description: ["등록하기 버튼을 클릭해서", "장소 정보 등록을 시작해주세요."],
+    source: "/guide/register/singleConquerGuide/singleConquerGuide03.png",
+    sourceType: "image",
+  },
+]
+
+// 여러 곳 정복하기
+export const multipleConquerGuide: GuideSlideContent[] = [
+  {
+    description: ["지도 버튼을 클릭해서, 지도 화면에 진입해주세요."],
+    source: "/guide/register/multipleConquerGuide/multipleConquerGuide01.png",
+    sourceType: "image",
+  },
+  {
+    description: ["원하는 지역으로 지도를 이동 시킨 후", "음식점/카페/편의점 등 카테고리를 선택해주세요."],
+    source: "/guide/register/multipleConquerGuide/multipleConquerGuide02.png",
+    sourceType: "image",
+  },
+  {
+    description: ["원하는 지역으로 지도를 이동 시킨 후", "음식점/카페/편의점 등 카테고리를 선택해주세요."],
+    source: "/guide/register/multipleConquerGuide/multipleConquerGuide03.png",
+    sourceType: "image",
+  },
+  {
+    description: ["등록하기 버튼을 클릭해서", "장소 정보 등록을 시작해주세요."],
+    source: "/guide/register/multipleConquerGuide/multipleConquerGuide04.png",
+    sourceType: "image",
+  },
+]
+
+// 매장 정보 입력하기
+export const placeConquerGuide: GuideSlideContent[] = [
+  {
+    description: ["층수를 입력해주세요."],
+    source: "/guide/register/placeConquerGuide/placeConquerGuide01.png",
+    sourceType: "image",
+    extraDescription: {
+      title: "💡 주의사항️",
+      description: ["1-2층을 모두 포함해요:", "단독건물이거나, 1층을 포함해서 여러층을 사용하는 경우에 선택해주세요."],
+    },
+  },
+  {
+    description: ["사진 가이드를 참고해서,", "매장 입구 사진을 찍어주세요."],
+    source: "/guide/register/placeConquerGuide/placeConquerGuide02.png",
+    sourceType: "image",
+    extraDescription: {
+      title: "💡 주의사항️",
+      description: ["매장이 1층이 아닌 경우", "해당 층에서 매장의 입구 사진을 촬영해주세요!"],
+    },
+  },
+  {
+    description: ["계단 또는 경사로 정보를 입력해주세요.", "*계단이 1칸이라면 높이도 함께 입력해요."],
+    source: placeConquerGuide03,
+    sourceType: "lottie",
+    extraDescription: {
+      title: "💡 계단 높이 정보가 필요한 이유?",
+      description: ["이용자의 상황에 따라 접근 가능한 턱 높이가 다를 수 있어요."],
+    },
+  },
+  {
+    description: ["출입문 유형을 알려주세요.", "중복 선택이 가능합니다."],
+    source: placeConquerGuide04,
+    sourceType: "lottie",
+  },
+  {
+    description: ["더 도움이 될 정보가 있다면,", "의견을 남겨주세요."],
+    source: placeConquerGuide05,
+    sourceType: "lottie",
+    extraDescription: {
+      title: "🍀 코멘트는 접근성을 판단하는 데 큰 도움이 돼요.",
+      description: [
+        "예: “이 사진은 경사로가 있는 후문 기준입니다.”",
+        "“경사로가 있지만 좁고 가파른 편이라 전동휠체어 이용자는 사진을 꼭 확인해주세요.”",
+      ],
+      descriptionStyle: "disc",
+    },
+  },
+]
+
+// 건물 정보 입력하기
+export const buildingConquerGuide: GuideSlideContent[] = [
+  {
+    description: ["건물 입구 사진을 촬영해주세요."],
+    source: "/guide/register/buildingConquerGuide/buildingConquerGuide01.png",
+    sourceType: "image",
+    extraDescription: {
+      title: "💡 건물 입구가 여러개인 경우라면?",
+      description: [
+        "건물 입구가 여러 개인 경우, 이동약자가 접근할 수 있는 문을 기준으로 촬영해주세요.",
+        "확인이 어려운 경우에는 정문을 촬영하면 됩니다.",
+        "어떤 문을 기준으로 찍었는지 의견란에 적어주시면, 큰 도움이 됩니다.",
+      ],
+      descriptionStyle: "disc",
+    },
+  },
+  {
+    description: ["계단 또는 경사로 정보를 입력해주세요.", "*계단이 1칸이라면 높이도 함께 입력해요."],
+    source: buildingConquerGuide02,
+    sourceType: "lottie",
+    extraDescription: {
+      title: "💡 계단 높이 정보가 필요한 이유?",
+      description: ["이용자의 상황에 따라 접근 가능한 턱 높이가 다를 수 있어요."],
+    },
+  },
+  {
+    description: ["출입문 유형을 알려주세요.", "중복 선택이 가능합니다."],
+    source: buildingConquerGuide03,
+    sourceType: "lottie",
+  },
+  {
+    description: ["건물 엘리베이터 정보를 입력해주세요."],
+    source: buildingConquerGuide04,
+    sourceType: "lottie",
+    extraDescription: {
+      title: "💡 엘리베이터 탑승하러 가는 길 정보도 필요해요.",
+      description: [
+        "엘리베이터 유무뿐 아니라, 탑승하러 가는 길의 정보도 필요해요.",
+        "그 길에 계단이나 경사로가 있다면 꼭 함께 입력해주세요.",
+      ],
+    },
+  },
+  {
+    description: ["더 도움이 될 정보가 있다면,", "의견을 남겨주세요."],
+    source: buildingConquerGuide05,
+    sourceType: "lottie",
+    extraDescription: {
+      title: "🍀 코멘트는 접근성을 판단하는 데 큰 도움이 돼요.",
+      description: [
+        "예: “건물에 여러 입구가 있습니다. 후문 쪽이 훨씬 평평하고, 경사로도 있어서 추천해요.”",
+        "“지하 주차장에서 연결되는 엘리베이터를 이용하면 편리합니다.”",
+      ],
+      descriptionStyle: "disc",
+    },
+  },
+]
+//#endregion
+
+//#region 정보 조회하기
+export const locationSearchGuide: GuideSlideContent[] = [
+  {
+    description: ["지도 아이콘을 클릭해서,", "지도 화면으로 진입해주세요."],
+    source: "/guide/search/locationSearchGuide/locationSearchGuide01.png",
+    sourceType: "image",
+  },
+  {
+    description: ["원하는 지역으로 지도를 이동 시킨 후", "음식점/카페/편의점 등 카테고리를 선택해주세요."],
+    source: "/guide/search/locationSearchGuide/locationSearchGuide02.png",
+    sourceType: "image",
+  },
+  {
+    description: ["지도의 마커를 통해", "한눈에 접근성을 확인할 수 있어요."],
+    source: "/guide/search/locationSearchGuide/locationSearchGuide03.png",
+    sourceType: "image",
+  },
+  {
+    description: ["장소 상세페이지에 들어가면", "사진과 함께 자세한 접근성 정보를 볼 수 있어요."],
+    source: "/guide/search/locationSearchGuide/locationSearchGuide04.png",
+    sourceType: "image",
+  },
+]
+
+export const placeSearchGuide: GuideSlideContent[] = [
+  {
+    description: ["검색창을 눌러, 검색 화면으로 들어가세요."],
+    source: "/guide/search/placeSearchGuide/placeSearchGuide01.png",
+    sourceType: "image",
+  },
+  {
+    description: ["장소 이름을 입력하고,", "검색 결과에서 원하는 매장을 선택하세요."],
+    source: "/guide/search/placeSearchGuide/placeSearchGuide02.png",
+    sourceType: "image",
+  },
+  {
+    description: ["장소 상세페이지에서 사진과 함께 접근성 정보를 확인할 수 있어요."],
+    source: "/guide/search/placeSearchGuide/placeSearchGuide03.png",
+    sourceType: "image",
+  },
+]
+
+export const filterSearchGuide: GuideSlideContent[] = [
+  {
+    description: ["카테고리나 장소를 검색해", "지도 화면으로 이동해주세요."],
+    source: "/guide/search/filterSearchGuide/filterSearchGuide01.png",
+    sourceType: "image",
+  },
+  {
+    description: ["상단의 검색 필터를 사용해", "조건에 맞는 장소만 골라볼 수 있어요."],
+    source: "/guide/search/filterSearchGuide/filterSearchGuide02.png",
+    sourceType: "image",
+  },
+  {
+    title: "접근 레벨",
+    description: ["접근 레벨에 따라", "원하는 수준만 골라볼 수 있어요."],
+    source: filterSearchGuide03,
+    sourceType: "lottie",
+  },
+  {
+    title: "경사로 유무",
+    description: ["접근레벨 0이거나,", "경사로가 있는 곳만 모아볼 수 있어요."],
+    source: filterSearchGuide04,
+    sourceType: "lottie",
+  },
+  {
+    title: "정복 유무",
+    description: ["정보가 등록된 곳만", "따로 모아서 볼 수 있어요."],
+    source: filterSearchGuide05,
+    sourceType: "lottie",
+  },
+]
+
+export const sccRoadGuide: GuideSlideContent[] = [
+  {
+    description: ["앱 하단에서 ‘메뉴' 탭을 클릭해요"],
+    source: "/guide/search/sccRoadGuide/sccRoadGuide01.png",
+    sourceType: "image",
+  },
+  {
+    description: ["[뿌클로드: 이동약자를 위한 진짜 리뷰]", "항목을 선택해요."],
+    source: "/guide/search/sccRoadGuide/sccRoadGuide02.png",
+    sourceType: "image",
+  },
+  {
+    description: ["이동약자와 친구로 구성된 에디터가 직접 다녀온 카페, 식당, 관광지 후기를 모아볼 수 있어요."],
+    source: "/guide/search/sccRoadGuide/sccRoadGuide03.png",
+    sourceType: "image",
+  },
+  {
+    description: ["휠체어 유형, 화장실 유무 등", "장소 방문 전 꼭 필요한 정보를 확인해보세요."],
+    source: sccRoadGuide04,
+    sourceType: "lottie",
+  },
+]
+//#endregion
