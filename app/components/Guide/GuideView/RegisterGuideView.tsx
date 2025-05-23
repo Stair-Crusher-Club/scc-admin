618c05323d2f558cc59c15a570b5e8811256577a
ixio0330 <ixio0330@gmail.com>
2025-05-23 21:57:32 +0900

    chore: 문구 변경 및 간격 조정

--- app/components/Guide/GuideView/RegisterGuideView.tsx
+++ app/components/Guide/GuideView/RegisterGuideView.tsx
@@ -11,16 +11,14 @@ export default function RegisterGuideView() {
         }}
       >
         <div style={{ padding: "0 20px", marginBottom: 20 }}>
-          <GuideTypography variant="title" css={{ marginBottom: 32 }}>
+          <GuideTypography variant="title" css={{ marginBottom: 20 }}>
             1. 장소 검색하기
           </GuideTypography>
-
           <GuideTypography variant="subtitle" css={{ marginBottom: 8 }}>
             장소 하나 정복하기
           </GuideTypography>
-          <GuideTypography variant="description">
-            정복(등록)하고 싶은 장소가 있다면, 검색창에 입력해서 바로 등록을 시작해요.
-          </GuideTypography>
+          <GuideTypography variant="description">정복(등록)하고 싶은 장소가 있다면,</GuideTypography>
+          <GuideTypography variant="description">검색창에 입력해서 바로 등록을 시작해요.</GuideTypography>
         </div>
 
         <GuideSlider items={singleConquerGuide} name="singleConquerGuide" />
@@ -30,8 +28,9 @@ export default function RegisterGuideView() {
             여러 곳 정복하기
           </GuideTypography>
           <GuideTypography variant="description">
-            [정복 안된 곳만 보기] 필터로 내 근처 미정복 장소를 찾아, 한 번에 여러 곳을 쉽게 정복할 수 있어요!
+            [정복 안된 곳만 보기] 필터로 내 근처 미정복 장소를 찾아,
           </GuideTypography>
+          <GuideTypography variant="description">한 번에 여러 곳을 쉽게 정복할 수 있어요!</GuideTypography>
         </div>
 
         <GuideSlider items={multipleConquerGuide} name="multipleConquerGuide" />
@@ -43,15 +42,15 @@ export default function RegisterGuideView() {
         }}
       >
         <div style={{ padding: "0 20px", marginBottom: 20 }}>
-          <GuideTypography variant="title" css={{ marginBottom: 32 }}>
+          <GuideTypography variant="title" css={{ marginBottom: 20 }}>
             2. 계단정보 등록하기
           </GuideTypography>
           <GuideTypography variant="subtitle" css={{ marginBottom: 8 }}>
             매장 정보 입력하기
           </GuideTypography>
-          <GuideTypography variant="description">
-            층수, 출입구 사진, 계단 정보 순서로 입력해요. 정확한 사진을 등록할수록, 다른 사람들에게 더 큰 도움이 됩니다.
-          </GuideTypography>
+          <GuideTypography variant="description">층수, 출입구 사진, 계단 정보 순서로 입력해요.</GuideTypography>
+          <GuideTypography variant="description">정확한 사진을 등록할수록,</GuideTypography>
+          <GuideTypography variant="description">다른 사람들에게 더 큰 도움이 됩니다.</GuideTypography>
         </div>
 
         <GuideSlider items={placeConquerGuide} name="placeConquerGuide" />
@@ -61,11 +60,10 @@ export default function RegisterGuideView() {
             건물 정보 입력하기
           </GuideTypography>
           <GuideTypography variant="description">
-            매장 정보를 다 입력했다면, 꼭 건물 정보도 함께 등록해주세요.
+            매장 정보를 다 입력했다면, 꼭 <strong>건물 정보</strong>도 함께 등록해주세요.
           </GuideTypography>
-          <GuideTypography variant="description">
-            건물 정보가 없으면 전체적인 접근성 정보를 정확히 전달할 수 없어요.
-          </GuideTypography>
+          <GuideTypography variant="description">건물 정보가 없으면 전체적인 접근성 정보를</GuideTypography>
+          <GuideTypography variant="description">정확히 전달할 수 없어요.</GuideTypography>
         </div>
 
         <GuideSlider items={buildingConquerGuide} name="buildingConquerGuide" />
