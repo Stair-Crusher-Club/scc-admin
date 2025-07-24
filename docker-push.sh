19d6a7f1c2b10202b37bd744351c9109bb0d96b9
Jason Yoo <jasonyoo0515@gmail.com>
2025-07-24 14:47:36 +0900

    feat: migrate to private ecr repo (#61)

--- docker-push.sh
+++ docker-push.sh
@@ -12,7 +12,7 @@ if [[ $1 = "dev" ]]; then
   IMAGE_TAG="$IMAGE_TAG-rc"
 fi;
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/q0g6g7m8
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 291889421067.dkr.ecr.ap-northeast-2.amazonaws.com
 
-docker buildx build --platform linux/amd64 -t public.ecr.aws/i6n1n6v2/scc-admin-frontend:$IMAGE_TAG --build-arg="NEXT_PUBLIC_DEPLOY_TYPE=$1" .
-docker push public.ecr.aws/i6n1n6v2/scc-admin-frontend:$IMAGE_TAG
+docker buildx build --platform linux/amd64 -t 291889421067.dkr.ecr.ap-northeast-2.amazonaws.com/scc-admin-frontend:$IMAGE_TAG --build-arg="NEXT_PUBLIC_DEPLOY_TYPE=$1" .
+docker push 291889421067.dkr.ecr.ap-northeast-2.amazonaws.com/scc-admin-frontend:$IMAGE_TAG
