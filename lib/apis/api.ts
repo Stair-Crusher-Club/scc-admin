eb64fbcaa09fd95298eef721704bc697f125a72a
Zeniuus <jsh56son@gmail.com>
2025-11-03 15:28:02 +0900

    Fix: host

--- lib/apis/api.ts
+++ lib/apis/api.ts
@@ -35,8 +35,8 @@ import {
 const baseURL =
   process.env.NEXT_PUBLIC_DEPLOY_TYPE === "live"
     ? "https://api.staircrusher.club/admin"
-    // : "https://api.dev.staircrusher.club/admin"
-    : "http://localhost:8080/admin"
+    : "https://api.dev.staircrusher.club/admin"
+    // : "http://localhost:8080/admin"
 const config = new Configuration({ basePath: baseURL })
 const defaultApi = new DefaultApi(config)
 const challengeApi = new ChallengeApi(config)
