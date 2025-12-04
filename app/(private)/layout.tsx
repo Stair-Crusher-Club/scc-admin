3db1727101c0ca52405912966284ebd63a399790
sanggggg <ksme6776@gmail.com>
2025-12-05 02:24:41 +0900

    fix: scroll issue

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -39,9 +39,9 @@ export default function PrivateLayout({ children }: { children: React.ReactNode 
       <AppSidebar variant="inset" />
       <SidebarInset>
         <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <div className="flex flex-1 flex-col overflow-x-hidden">
+          <div className="@container/main flex flex-1 flex-col gap-2 overflow-x-hidden">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 overflow-x-hidden">
               {children}
             </div>
           </div>
